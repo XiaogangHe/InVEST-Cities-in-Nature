1a32760a92aefc408109d93d7246099af3a98a20
Xiaogang HE <xiaoganghe@Xiaogangs-MacBook-Air.local>
2022-12-17 15:19:53 +0800

    update Ecosystem Services part

--- website/websiteData/features.js
+++ website/websiteData/features.js
@@ -5,60 +5,26 @@ const features = [
     description: (
       <ul className="features__list">
         <li>
-          <a href="/docs/">Introduction to moja global</a>
+          <a href="/docs/">Introduction to InVEST</a>
         </li>
         <li>
-          <a href="/docs/about-moja-global">About moja global</a>
-        </li>
-        <li>
-          <a href="/community/technical-steering-committee">Technical Steering Committee</a>
-        </li>
-        <li>
-          <a href="/community/governance">Governance model</a>
-        </li>
-        <li>
-          <a href="/community/working-groups/doc-wg">Working Groups</a>
+          <a href="/docs/about-moja-global">About InVEST</a>
         </li>
       </ul>
     ),
   },
   {
-    title: 'Projects',
+    title: 'Models',
     description: (
       <ul className="features__list">
         <li>
-          <a href="/docs/FLINT/About">FLINT</a>
+          <a href="/docs/FLINT/About">Urban Cooling</a>
         </li>
         <li>
-          <a href="/docs/FLINT-Cloud/About">FLINT Cloud</a>
+          <a href="/docs/FLINT-Cloud/About">Urban Flooding</a>
         </li>
         <li>
-          <a href="/docs/FLINT-Reporting-Tool/About">FLINT Reporting Tool</a>
-        </li>
-        <li>
-          <a href="/docs/FLINT-UI/About">FLINT Visualization Tool</a>
-        </li>
-        <li>
-          <a href="/docs/GCBM/About">GCBM</a>
-        </li>
-      </ul>
-    ),
-  },
-  {
-    title: 'Contributing',
-    description: (
-      <ul className="features__list">
-        <li>
-          <a href="/community/contributing">Contributing guidelines</a>
-        </li>
-        <li>
-          <a href="/community/code-contribution">Code contributions</a>
-        </li>
-        <li>
-          <a href="/community/docs-contribution">Documentation contributions</a>
-        </li>
-        <li>
-          <a href="/community/management-contribution">Management contributions</a>
+          <a href="/docs/FLINT-Reporting-Tool/About">Carbon Sequestration</a>
         </li>
       </ul>
     ),
@@ -96,56 +62,18 @@ const features = [
     ),
   },
   {
-    title: 'Mentorship & Initiatives',
-    description: (
-      <ul className="features__list">
-        <li>
-          <a href="/initiatives">Community initiatives</a>
-        </li>
-        <li>
-          <a href="/community/mentorship#outreachy-internships">Outreachy internships</a>
-        </li>
-        <li>
-          <a href="/community/mentorship#google-season-of-docs">Google Season of Docs</a>
-        </li>
-        <li>
-          <a href="/community/mentorship#google-summer-of-code">Google Summer of Code</a>
-        </li>
-      </ul>
-    ),
-  },
-  {
-    title: 'Safety & Support',
-    description: (
-      <ul className="features__list">
-        <li>
-          <a href="/community/code-of-conduct">Community Code of Conduct</a>
-        </li>
-        <li>
-          <a href="/community/support">Community support</a>
-        </li>
-      </ul>
-    ),
-  },
-  {
     title: 'Documentation',
     description: (
       <ul className="features__list">
         <li>
-          <a href="https://docs.moja.global/">moja global technical documentation</a>
+          <a href="https://docs.moja.global/">InVEST Cities in Nature Website</a>
         </li>
         <li>
-          <a href="https://docs.moja.global/projects/flintcloud/en/latest/">FLINT Cloud</a>
-        </li>
-        <li>
-          <a href="https://docs.moja.global/projects/flint-reporting/en/latest/">FLINT Reporting</a>
-        </li>
-        <li>
-          <a href="https://docs.moja.global/projects/flint-ui/en/latest/">FLINT UI</a>
+          <a href="https://docs.moja.global/projects/flint-reporting/en/latest/">InVEST Reporting</a>
         </li>
         <li>
           <a href="https://docs.moja.global/projects/GCBM-Chile-Data-Preprocessing-Tools/en/latest/">
-            GCBM Chile Pre-Processing Tools
+            InVEST Land Use Scenario Generation Tools
           </a>
         </li>
       </ul>
@@ -156,8 +84,8 @@ const features = [
     description: (
       <ul className="features__list">
         <li>
-          <a href="https://www.researchgate.net/publication/341041237_Modelling_forest_carbon_dynamics_for_REDD_using_the_Generic_Carbon_Budget_Model_GCBM_Pilot_Project_Los_Rios_Region_-_Chile">
-            Modelling forest carbon dynamics for REDD+
+          <a href="https://www.science.org/doi/10.1126/sciadv.abe6025">
+            Flood Managed Aquifer Recharge
           </a>
         </li>
         <li>
@@ -174,9 +102,6 @@ const features = [
       <ul className="features__list">
         <li>
           <a href="https://docs.moja.global/en/latest/faq.html">Frequently Asked Questions</a>
-        </li>
-        <li>
-          <a href="https://docs.moja.global/en/latest/contact.html">Join moja global</a>
         </li>
         <li>
           <a href="/tutorial/tutorial-introduction">Tutorials</a>
