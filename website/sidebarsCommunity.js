a0c084810eec7d3691745972bb91e61335c67aa6
Xiaogang HE <xiaoganghe@Xiaogangs-MacBook-Air.local>
2022-12-18 21:08:44 +0800

    delete unsed files

--- website/sidebarsCommunity.js
+++ website/sidebarsCommunity.js
@@ -3,12 +3,11 @@ module.exports = {
     'support',
     'contributing',
     'community-website-contributions',
-    'community-contribution',
     {
       type: 'category',
       label: 'Working Groups',
       items: ['working-groups/doc-wg', 'working-groups/ui-wg'],
     },
-    'strategic-plan-change',
+    'community-contribution',
   ],
 };
