2dfc81fb0a215f31dc5547d34c28d7f9322f1c46
Xiaogang HE <xiaoganghe@Xiaogangs-MacBook-Air.local>
2022-12-28 09:30:01 +0800

    update

--- website/sidebarsCaseStudies.js
+++ website/sidebarsCaseStudies.js
@@ -2,6 +2,5 @@ module.exports = {
     community: [
         'case-studies-introduction',
         'powering-deforestation-prediction-using-artificial-intelligence',
-        'introduction-to-invest-urban-flooding',
     ]
 }
