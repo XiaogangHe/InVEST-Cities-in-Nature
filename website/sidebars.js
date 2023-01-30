d90005d5ad6ba2e8d71c4b8f06bf7f543847f2fb
Xiaogang HE <xiaoganghe@Xiaogangs-MacBook-Air.local>
2023-01-30 18:58:54 +0800

    add misc

--- website/sidebars.js
+++ website/sidebars.js
@@ -11,19 +11,19 @@ module.exports = {
       type: 'category',
       collapsed: true,
       label: 'Flood Risk Mitigation',
-      items: ['Flood-Risk-Mitigation/about', 'Flood-Risk-Mitigation/data'],
+      items: ['Flood-Risk-Mitigation/about', 'Flood-Risk-Mitigation/data', 'Flood-Risk-Mitigation/misc'],
     },
     {
       type: 'category',
       collapsed: true,
       label: 'Coastal Vulnerability',
-      items: ['Coastal-Vulnerability/about', 'Coastal-Vulnerability/data'],
+      items: ['Coastal-Vulnerability/about', 'Coastal-Vulnerability/data', 'Coastal-Vulnerability/misc'],
     },
     {
       type: 'category',
       collapsed: true,
       label: 'Carbon Sequestration',
-      items: ['Carbon-Storage-and-Sequestration/about', 'Carbon-Storage-and-Sequestration/data'],
+      items: ['Carbon-Storage-and-Sequestration/about', 'Carbon-Storage-and-Sequestration/data', 'Carbon-Storage-and-Sequestration/misc'],
     },
   ],
 };
